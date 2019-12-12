14c25bca119f04730cdf1ab28ac1ed8126612d65
ghd075 <rlfls54@gmail.com>
2019-12-13 02:10:10 +0900

    junit4 테스트 라이브러리 기본 사용법!!

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -2,13 +2,34 @@ package calculator;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
 
+	private Calculator cal;
+
+	@Before
+	public void setup() {
+		cal = new Calculator();
+		System.out.println("setup");
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void add() {
+		assertEquals(3, cal.add(2, 1));
+		System.out.println("add");
 	}
 
+	@Test
+	public void divide() {
+		assertEquals(3, cal.divide(9, 3));
+		System.out.println("divide");
+	}
+	
+	@After
+	public void teardown() {
+		System.out.println("teardown");
+	}
 }
