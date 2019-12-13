43789382a16c50799fcce32ceadf0b5ffff7b9c0
ghd075 <rlfls54@gmail.com>
2019-12-14 02:03:40 +0900

    요구사항3 리팩토링!!

--- src/main/java/calculator/StringCaculator.java
+++ src/main/java/calculator/StringCaculator.java
@@ -6,34 +6,27 @@ package calculator;
  *
  */
 public class StringCaculator {
-	
+
 	public int add(String text) {
 		/*
 		 * 요구사항1 빈 문자열 또는 null값을 입력할 경우 0을 반환
 		 * 
-		 * */
-		if(text == null || text.isEmpty()) {
+		 */
+		if (text == null || text.isEmpty()) {
 			return 0;
 		}
-		
+
 		/*
 		 * 요구사항3 숫자 두개를 쉼표(,)구분자로 입력할 경우 두 숫자의 합을 반환
 		 * 
-		 * */
-		if(text.contains(",")) {
-			String[] values = text.split(",");
-			int sum = 0;
-			for(String value : values) {
-				sum += Integer.parseInt(value);
-			}
-			return sum;
+		 */
+		String[] values = text.split(",");
+		int sum = 0;
+		for (String value : values) {
+			sum += Integer.parseInt(value);
 		}
 		
-		/*
-		 * 요구사항2 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.
-		 * 
-		 * */
-		return Integer.parseInt(text);
+		return sum;
+
 	}
-	
 }
