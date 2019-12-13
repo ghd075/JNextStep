6e3eb0bf138de64374a915a4df772d694a58fe53
ghd075 <rlfls54@gmail.com>
2019-12-14 02:19:19 +0900

    요구사항3코드 리팩토링!!

--- src/main/java/calculator/StringCaculator.java
+++ src/main/java/calculator/StringCaculator.java
@@ -12,21 +12,46 @@ public class StringCaculator {
 		 * 요구사항1 빈 문자열 또는 null값을 입력할 경우 0을 반환
 		 * 
 		 */
-		if (text == null || text.isEmpty()) {
+		if (isBlank(text)) {
 			return 0;
 		}
-
-		/*
-		 * 요구사항3 숫자 두개를 쉼표(,)구분자로 입력할 경우 두 숫자의 합을 반환
-		 * 
-		 */
+		
+		return sum(toInts(split(text)));
+	}
+	
+	//요구사항1 리팩토링로 분리
+	private boolean isBlank(String text) {
+		return text == null || text.isEmpty();
+	}
+	
+	/*
+	 * 요구사항2 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환
+	 * 요구사항3 숫자 두개를 쉼표(,)구분자로 입력할 경우 두 숫자의 합을 반환
+	 * 
+	 */
+	
+	//리팩토링로 split기능 분리
+	private String[] split(String text) {
 		String[] values = text.split(",");
+		return values;
+	}
+	
+	//문자열 -> 숫자로 변환하는 기능 분리
+	private int[] toInts(String[] values) {
+		int[] numbers = new int[values.length];
+		for(int i = 0; i < values.length; i++) {
+			numbers[i] = Integer.parseInt(values[i]);
+		}
+		return numbers;
+	}
+	
+	//리팩토링로 sum기능 분리
+	private int sum(int[] numbers) {
 		int sum = 0;
-		for (String value : values) {
-			sum += Integer.parseInt(value);
+		for (int number : numbers) {
+			sum += number;
 		}
 		
 		return sum;
-
 	}
 }
